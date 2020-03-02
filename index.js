80788aa3d359ed26281f6f139a45029b12432937
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2020-03-03 09:24:46 +1300

    Update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var num1 = 1;
 var num2 = 3;
-console.log(num1 + num2);
-console.log(num1 - num2);
-console.log(num1 / num2);
-console.log(num1 % num2);
-console.log(num1 * num2);
+console.log("Add:",num1 + num2);
+console.log("Sub:",num1 - num2);
+console.log("Div:",num1 / num2);
+console.log("Per:",num1 % num2);
+console.log("Mul:",num1 * num2);
